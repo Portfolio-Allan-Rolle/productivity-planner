3e5c9ff23fe01cb93986a5c5be2b7b2a5f2b6114
allan1989 <allan.rolle@laposte.net>
2025-04-17 18:50:05 +0200

    add comment to allow environment variables

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  url: 'https://console.firebase.google.com/u/0/project/productivity-planner-dev-7b8b1/overview',
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBtNvgtfG0uSP83W-qz-9_K2l4LtZBt47w',
+    authDomain: 'productivity-planner-dev-7b8b1.firebaseapp.com',
+    projectId: 'productivity-planner-dev-7b8b1',
+    storageBucket: 'productivity-planner-dev-7b8b1.firebasestorage.app',
+    messagingSenderId: '1025222613493',
+    appId: '1:1025222613493:web:34da9e40d7d72f4aeb68bc',
+  },
+};
