bf02358db998b46835268fac0ba0ac860056dcdf
allan1989 <allan.rolle@laposte.net>
2025-04-23 13:17:59 +0200

    WIP

--- src/app/core/auth.service.spec.ts
+++ src/app/core/auth.service.spec.ts
@@ -1,9 +1,16 @@
-// import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+import { AuthService } from './auth.service';
 
-// describe('AuthService', () => {
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       providers: [],
-//     });
-//   });
-// });
+describe('AuthService', () => {
+  let service: AuthService;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [AuthService],
+    });
+    service = TestBed.inject(AuthService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
